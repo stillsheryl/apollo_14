4bc37af1f7d8ad9c619e955a97443980ac10b9bd
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 06:44:47 -0600

    Add average age to index

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,4 +3,8 @@ class Astronaut < ApplicationRecord
 
   has_many :astronaut_missions
   has_many :missions, through: :astronaut_missions
+
+  def self.average_age
+    Astronaut.average(:age).round(1)
+  end
 end
