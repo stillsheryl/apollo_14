076e1ec07550ecb309e18a50bbd788684021e0eb
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 06:58:04 -0600

    Add test can list missions

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -33,7 +33,7 @@ describe "As a visitor," do
       expect(page).to have_content("#{astronaut_3.job}")
     end
 
-    it "Shows the Average Age of all astronauts" do
+    it "I see the Average Age of all astronauts" do
       astronaut_1 = Astronaut.create!(
                                 name: "Neil Armstrong",
                                 age: 37,
@@ -52,6 +52,43 @@ describe "As a visitor," do
       visit '/astronauts'
 
       expect(page).to have_content("Average Age: 36.3")
+    end
+
+    it "I see a list of the space missions' in alphabetical order for each astronaut. (e.g 'Apollo 13'
+                         'Capricorn 4'
+                         'Gemini 7')" do
+      astronaut_1 = Astronaut.create!(
+                                name: "Neil Armstrong",
+                                 age: 37,
+                                 job: "Commander"
+                              )
+      astronaut_2 = Astronaut.create!(
+                                name: "Bob Hope",
+                                 age: 32,
+                                 job: "Mission Control"
+                              )
+      astronaut_3 = Astronaut.create!(
+                                name: "Shana Young",
+                                 age: 40,
+                                 job: "Engineer"
+                              )
+      mission_1 = Mission.create!(
+                              title: "Apollo 13",
+                      time_in_space: 5436
+                              )
+      mission_2 = Mission.create!(
+                              title: "Capricorn 4",
+                      time_in_space: 25773
+                              )
+      mission_3 = Mission.create!(
+                              title: "Gemini 7",
+                              time_in_space: 579276
+                              )
+      visit '/astronauts'
+
+      expect(page).to have_content('Apollo 13')
+      expect(page).to have_content('Capricorn 4')
+      expect(page).to have_content('Gemini 7')
     end
   end
 
